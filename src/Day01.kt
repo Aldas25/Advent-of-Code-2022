311c44ae77c2348229e14137eb8f9ff67919afd8
Aldas25 <aldas.lenksas@gmail.com>
2022-12-02 07:02:43 +0200

    Day 1 solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,49 @@
+import com.sun.jdi.IntegerType
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var answer = 0
+        var currentElf = 0
+
+        for (s in input) {
+            if (s.isBlank()) {
+                currentElf = 0
+            } else {
+                currentElf += s.toInt()
+            }
+
+            answer = maxOf(answer, currentElf)
+        }
+
+        return answer
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var currentElf = 0
+        var listOfElfs: List<Int> = emptyList()
+
+        for (s in input) {
+            if (s.isBlank()) {
+                listOfElfs = listOfElfs.plus(currentElf)
+                currentElf = 0
+            } else {
+                currentElf += s.toInt()
+            }
+        }
+        listOfElfs = listOfElfs.plus(currentElf)
+        listOfElfs = listOfElfs.sortedDescending()
+
+        val answer = listOfElfs[0] + listOfElfs[1] + listOfElfs[2]
+
+        return answer
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val filename =
+//        "inputs\\day01_sample"
+        "inputs\\day01"
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput(filename)
+
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
